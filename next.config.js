64400c3ced8e931e56702fbf249d8066652a79f4
Shaik Dadapeer <69897636+DaduDev@users.noreply.github.com>
2025-01-02 13:30:02 +0530

    resolving bug 01

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.mode = 'production'; 
+    return config;
+  },
 }
 
 module.exports = nextConfig
